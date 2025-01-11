b14cdc9e500d5dcb3661136bf2906b7af86b148c
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-01-11 21:57:10 +0900

    feat: ProductモデルからBookモデルに変更したので、それに伴う修正と、検索機能を実際に伝えるように修正

--- search_app/views.py
+++ search_app/views.py
@@ -1,87 +1,89 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import SearchForm, ProductForm
-from .models import Product, Category
+from .forms import SearchForm, BookForm
+from .models import Book, Category
+from .utils import get_cover_image_url
+from django.db.models import Q
 
-def product_create(request):
+def book_create(request):
   if request.method == 'POST':
-    form = ProductForm(request.POST)
+    form = BookForm(request.POST)
     if form.is_valid():
       form.save()
-      return redirect('product_list')
+      return redirect('search_book_list')
     else:
-      form = ProductForm()
-    return render(request, 'product_form.html', {'form': form})
+      return render(request, 'book_form.html', {'form': form})
+  else:
+      form = BookForm()
+      return render(request, 'book_form.html', {'form': form})
 
 
-def product_detail(request, pk):
-  product = get_object_or_404(Product, pk=pk)
-  return render(request, 'product_detail.html', {'product': product})
+def book_detail(request, pk):
+  book = get_object_or_404(Book, pk=pk)
+  return render(request, 'book_detail.html', {'book': book})
 
 
-def product_update(request, pk):
-  product = get_object_or_404(Product, pk=pk)
+def book_update(request, pk):
+  book = get_object_or_404(Book, pk=pk)
   if request.method == 'POST':
-    form = ProductForm(request.POST, instance=product)
+    form = BookForm(request.POST, instance=book)
     if form.is_valid():
-      form.sava()
-      return redirect('product_detail', pk=product.pk)
+      form.save()
+      return redirect('book_detail', pk=book.pk)
     else:
-      form = ProductForm(instance=product)
+      return render(request, 'book_form.html', {'form': form, 'book': book})
 
-    return render(request, 'product_form.html', {'form': form, 'product': product})
+  else:
+      form = BookForm(instance=book)
+      return render(request, 'book_form.html', {'form': form, 'book': book})
 
 
-def product_delete(request, pk):
-  product = get_object_or_404(Product, pk=pk)
+def book_delete(request, pk):
+  book = get_object_or_404(Book, pk=pk)
   if request.method == 'POST':
-    product.delete()
-    return redirect('product_list')
-  return render(request, 'product_confirm_delete.html', {'product': product})
+    book.delete()
+    return redirect('search_book_list')
+  return render(request, 'book_confirm_delete.html', {'book': book})
 
 
-def product_list(request):
-  products = Product.objects.all()
-  return render(request, 'product_list.html', {'products': products})
-
-
-def search_view(request):
+def search_book_list(request):
   form = SearchForm(request.GET or None)
-  results = Product.objects.all()
+  results = Book.objects.all()
   if form.is_valid():
     query = form.cleaned_data['query']
-    if query:
-      results = results.filter(name__icontains=query)
+    print(f"query: {query}")
 
+    if query:
+      results = results.filter(Q(title__icontains=query) |
+                Q(author__icontains=query) |
+                Q(description__icontains=query))
+      print(f"results: {results}")
   # カテゴリフィルタリング
-  category_name = request.GET.get('category')
-  if category_name:
+  category_id = request.GET.get('category')
+  print(f"category_id: {category_id}")
+  if category_id == 'placeholder':
+    pass
+  elif category_id:
     try:
-      category = Category.objects.get(name=category_name)
+      category = Category.objects.get(id=category_id)
+      print(f"category: {category}")
       results = results.filter(category_id=category.id)
+      print(f"results: {results}")
     except Category.DoesNotExist:
       results = results.none()
       category = None
 
-
-  # 価格のフィルタリング（最低価格・最高価格）
-  min_price = request.GET.get('min_price')
-  max_price = request.GET.get('max_price')
-  if min_price:
-    results = results.filter(price__gte=min_price)
-  if max_price:
-    results = results.filter(price__gte=max_price)
-
   # 並び替え処理
   sort_by = request.GET.get('sort', 'name')
-  if sort_by == 'price_asc':
-    results = results.order_by('price')
-  elif sort_by == 'price_desc':
-    results = results.order_by('-price')
+  print(f"sort_by: {sort_by}")
+  if sort_by == 'title':
+    results = results.order_by('title')
+  elif sort_by == 'publication_date':
+    results = results.order_by('-publication_date')
 
   # クエリセットをリストに変換せず、直接Paginatorに渡す
   paginator = Paginator(results, 10)
   page_number = request.GET.get('page')
   page_obj = paginator.get_page(page_number)
-  return render(request, 'search.html', {'form': form, 'page_obj': page_obj, 'results': results})
+  return render(request, 'search_book_list.html', {'form': form, 'page_obj': page_obj, 'results': results})
