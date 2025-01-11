6d652b452f7a685e89dc50767f7545f79a5c9a39
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-01-11 21:53:17 +0900

    feat: ProductモデルからBookモデルに変更したので、それに伴う修正

--- search_app/models.py
+++ search_app/models.py
@@ -1,19 +1,41 @@
 from django.db import models
+from .utils import get_cover_image_url
 
 class Category(models.Model):
-  name = models.CharField(max_length=255)
+  name = models.CharField(max_length=255, unique=True)
 
 
   def __str__(self):
     return self.name
+  
+  @staticmethod
+  def create_default_categories():
+    default_categories = [
+      'IT・テクノロジー', '金融', '製造', '小売', 'サービス', '医療', '建設', 'エネルギー', '農業', 'その他'
+    ]
+    for category_name in default_categories:
+      Category.objects.get_or_create(name=category_name)
 
 
-class Product(models.Model):
-  name = models.CharField(max_length=100)
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.CharField(max_length=100)
+  publisher = models.CharField(max_length=100)
+  publication_date = models.DateField()
   description = models.TextField()
-  price = models.DecimalField(max_digits=10, decimal_places=2)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+  isbn = models.CharField(unique=True, default='')
 
 
   def __str__(self):
-    return self.name
+    return self.title
+
+  # def cover_url(self):
+  #   ISBNからOpen Libraryの画像URLを生成する
+  #   return f"https://covers.openlibrary.org/b/isbn/{self.isbn}-M.jpg"
+
+  def cover_url(self):
+    cover_url = get_cover_image_url(self.isbn)
+    return cover_url
+    
+
