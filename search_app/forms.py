fb44c6de220a80e3a62f0b2de1d96cc9cc264143
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-01-19 23:31:09 +0900

    feat: 書籍登録時のフォームのラベルや入力方法などをカスタマイズした

--- search_app/forms.py
+++ search_app/forms.py
@@ -6,11 +6,35 @@ class SearchForm(forms.Form):
     label='検索キーワード',
     max_length=100,
     required=False,
-    widget=forms.TextInput(attrs={'placeholder': '検索したいキーワードを入力'})
+    widget=forms.TextInput(attrs={'placeholder': 'キーワードを入力'})
   )
 
 
 class BookForm(forms.ModelForm):
   class Meta:
     model = Book
-    fields = ['title', 'author', 'publisher', 'publication_date', 'description', 'category', 'isbn'] 
+    fields = ['title', 'author', 'publisher', 'publication_date', 'description', 'category', 'isbn']
+    labels = {
+            'title': '書籍名',
+            'author': '著者',
+            'publisher': '出版社',
+            'publication_date': '出版日',
+            'description': '概要',
+            'category': 'カテゴリー',
+            'isbn': 'ISBNコード',
+        }
+    widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'タイトルを入力'}),
+            'author': forms.TextInput(attrs={'placeholder': '著者を入力'}),
+            'publisher': forms.TextInput(attrs={'placeholder': '出版社を入力'}),
+            'publication_date': forms.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': 4, 'placeholder': '書籍の概要を入力'}),
+            'category': forms.Select(attrs={'class': 'custom-select'}),
+            'isbn': forms.TextInput(attrs={'placeholder': '13桁のISBNコードを入力'})
+        }
+
+    def clean_isbn(self):
+        isbn = self.cleaned_data.get('isbn')
+        if len(isbn) != 13:
+            raise forms.ValidationError('ISBNは13桁である必要があります。')
+        return isbn
