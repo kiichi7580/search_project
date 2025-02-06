1c00eea9807702320f1db3278c45dd7bc0a151a5
kiichi7580 <kiichi.nakazato0521@gmail.com>
2025-02-07 01:16:17 +0900

    fix: インデント修正

--- search_app/utils.py
+++ search_app/utils.py
@@ -6,13 +6,13 @@ def get_cover_image_url(isbn):
     print(f"status_code: {response.status_code}")
 
     if response.status_code == 200:
-      data = response.json()
-      print(f"data: {data}")
+        data = response.json()
+        print(f"data: {data}")
 
-      thumbnail_url = data.get("items", [{}])[0].get("volumeInfo", {}).get("imageLinks", {}).get("thumbnail", "No image available")
+        thumbnail_url = data.get("items", [{}])[0].get("volumeInfo", {}).get("imageLinks", {}).get("thumbnail", "No image available")
 
-      print(thumbnail_url)
-      if thumbnail_url:
-          return thumbnail_url
-      else:
-          return None
+        print(thumbnail_url)
+        if thumbnail_url:
+            return thumbnail_url
+        else:
+            return None
